a1768190c289619f81c79e40854e6bc2ab75dac6
Teaom <dclarkjenkins87@gmail.com>
2023-05-21 16:09:01 -0600

    QA testing

--- index.js
+++ index.js
@@ -1,61 +1,74 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const {Circle, Square, Triangle} = require("./lib/shapes");
+const inquirer = require('inquirer')
+const fs = require('fs')
 
-function shapeFactory(shape, data){
-    switch(shape) {
-        case 'Circle':
-            return new Circle(data);
-        case 'Square':
-            return new Square(data);
-        case 'Triangle':
-            return new Triangle(data);    
-    }
+const { Circle, Triangle, Square} = require('./lib/shapes.js')
+
+function svgPrompt() {
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What shape would you like for your svg?',
+                name: 'shape',
+                choices: ['Circle', 'Square', 'Triangle'],
+            },
+            {
+                type: 'input',
+                message: 'What would color would you like the background to be? note: you can use a color name OR hexidecimal code (please include "#" if using hex)',
+                name: 'backgroundColor',
+                validate: function(input){
+                    if (input === '') {
+                        return `\x1b[31mPlease enter a color\x1b[0m` 
+                    }
+                    return true
+                }
+            },
+            {
+                type: 'input',
+                message: 'Enter your text (note: you can enter up to three characters):',
+                name:'svgText',
+                validate: function(input){
+                    if (input.length > 3) {
+                        return `\x1b[31mInput must be 3 characters or less\x1b[0m`
+                    }
+                    return true
+                }
+                
+            },
+            {
+                type: 'input',
+                message: 'What would color would you like the text to be (you can use a color name or hexidecimal code)?',
+                name: 'textColor',
+                validate: function(input){
+                    if (input === '') {
+                        return `\x1b[31mPlease enter a color\x1b[0m`
+                    }
+                    return true
+                }
+            },
+
+        ]).then((data) => {
+            const ShapeClass = {
+                'Triangle': Triangle,
+                'Circle': Circle,
+                'Square': Square
+            }[data.shape];
+        
+            const shape = new ShapeClass();
+            shape.backgroundColor = data.backgroundColor;
+            shape.svgText = data.svgText;
+            shape.textColor = data.textColor;
+        
+            fs.writeFile(`${data.shape.toLowerCase()}.svg`, shape.render(), (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`\x1b[32mGenerated ${data.shape.toLowerCase()}.svg\x1b[0m`);
+                }
+            });
+        })
+        
 }
 
-async function svgPrompt() {
-    const questions = [
-    {    
-        type: 'list',
-        message: 'What shape would you like',
-        name: 'shape',
-        choices: ['Circle', 'Square', 'Triangle'],
-    },      
-    {
-        type: 'input',
-        message: 'What color would you like for the background',
-        name: 'backgroundColor',
-        validate: function(input) {
-            return input ? true : `\x1b[31mPlease enter a color\x1b[Om`;
-        },
-    },
-    {
-        type: 'input',
-        message: 'Enter your text (note: you can enter up to three characters):',
-        name: 'svgText',
-        validate: function(input) {
-            return input.length <= 3 ? true : `\x1b[31mInput must be 3 characters or less\x1b[0m`;
-        },
-    },
-    {
-        type: 'input',
-        message: 'What color would you like the text to be (you can use a color name or hexadecimal code)?',
-        name: 'textColor',
-        validate: function(input) {
-        return input ? true : `\x1b[31mPlease enter a color\x1b[0m`;
-      },
-    },    
-    ];
-
-    const data = await inquirer.prompt(questions);
-    const shape = shapeFactory(data.shape, data);
-  
-    try {
-      await fs.writeFile(`${data.shape.toLowerCase()}.svg`, shape.render());
-      console.log(`\x1b[32mGenerated ${data.shape.toLowerCase()}.svg\x1b[0m`);
-    } catch (err) {
-      console.error(err);
-    }
-}
-
-svgPrompt();
+// Function call for the above function
+svgPrompt()
